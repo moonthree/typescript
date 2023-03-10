4c935114b1d8041ae614dc32e774df271be06b49
moonthree <fpdhslr0725@gmail.com>
2023-03-10 17:23:41 +0900

    a

--- TypeScriptPractice/src/components/myro/SectionBottom.tsx
+++ TypeScriptPractice/src/components/myro/SectionBottom.tsx
@@ -1,46 +1,43 @@
 import React from 'react';
 
+interface Item {
+  img: string;
+  title: string;
+  desc: string;
+}
+
+const arr: Item[] = [
+  { img: './jeju.jpg', title: 'JEJU', desc: '대한민국 제주도' },
+  { img: './jeju.jpg', title: 'JEJU', desc: '대한민국 제주도' },
+  { img: './jeju.jpg', title: 'JEJU', desc: '대한민국 제주도' },
+  { img: './jeju.jpg', title: 'JEJU', desc: '대한민국 제주도' },
+];
+
 export default function SectionBottom() {
   return (
     <div className="min-h-screen mx-auto max-w-7xl">
       <div className="text-3xl font-bold text-center">어디로 여행을 떠나시나요?</div>
       <div className="flex justify-around">
-        <div className="max-w-[250px] max-h-[500px] shadow-xl">
-          <div className="w-full h-full">
-            <img src="./jeju.jpg" alt="" className="hover:scale-110" />
-            <div className="m-5">
-              <div className="">JEJU</div>
-              <div className="">대한민국 제주도</div>
+        {arr.map((item: Item, index: number) => (
+          <div
+            key={index}
+            className="max-w-[250px] max-h-[500px] shadow-xl hover:cursor-pointer"
+          >
+            <div className="w-full h-full">
+              <div className="overflow-hidden">
+                <img
+                  src={item.img}
+                  alt=""
+                  className="transition-all duration-300 hover:scale-125"
+                />
+              </div>
+              <div className="m-5">
+                <div className="">{item.title}</div>
+                <div className="">{item.desc}</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="max-w-[250px] max-h-[500px] shadow-xl overflow-hidden">
-          <div className="w-full h-full">
-            <img src="./jeju.jpg" alt="" className="hover:scale-110" />
-            <div className="m-5">
-              <div className="">JEJU</div>
-              <div className="">대한민국 제주도</div>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-[250px] max-h-[500px] shadow-xl">
-          <div className="w-full h-full">
-            <img src="./jeju.jpg" alt="" className="hover:scale-110" />
-            <div className="m-5">
-              <div className="">JEJU</div>
-              <div className="">대한민국 제주도</div>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-[250px] max-h-[500px] shadow-xl">
-          <div className="w-full h-full">
-            <img src="./jeju.jpg" alt="" className="hover:scale-110" />
-            <div className="m-5">
-              <div className="">JEJU</div>
-              <div className="">대한민국 제주도</div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
