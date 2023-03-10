886c7271d693e42ebf9c6b9d2aa3df8444647677
moonthree <fpdhslr0725@gmail.com>
2023-03-10 17:47:14 +0900

    a

--- TypeScriptPractice/src/components/myro/Header.tsx
+++ TypeScriptPractice/src/components/myro/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="fixed left-0 right-0 flex justify-between mx-10">
+    <div className="fixed left-0 right-0 flex justify-between h-[130px] bg-white/70 backdrop-blur-2xl backdrop-filter">
       <div className="space-x-7">
         <span className="font-sans text-3xl font-extrabold">야 여기어때 놀자</span>
         <span>MAKE YOUR ROUTE OPTIMIZED</span>
       </div>
-      <div className="space-x-5 text-xl font-extrabold text-white">
+      <div className="space-x-5 text-xl font-extrabold">
         <span>여행지</span>
         <span>마이로</span>
         <span>이용방법</span>
